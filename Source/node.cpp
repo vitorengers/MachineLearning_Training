51c2e9ae01025c2c0be8884b32c6f0069de73e91
vitorengers <89425698+vitorengers@users.noreply.github.com>
2022-03-27 12:24:44 -0300

    Initial commit

---
+++ Source/node.cpp
@@ -0,0 +1,15 @@
+#include "node.h"
+
+template <typename T>
+Node<T>::Node()
+{
+
+}
+
+template <typename T>
+Node<T>::~Node()
+{
+
+}
+
+
---
+++ Source/node.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+template <typename T >
+class Node
+{
+public:
+    Node();
+    ~Node();
+    
+private:
+// protected:
+};
